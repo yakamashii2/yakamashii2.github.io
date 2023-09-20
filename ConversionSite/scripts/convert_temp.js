d8b99802a46a2a5b1b78e976d3a5de19b104f242
Edward Aldeen <edward.aldeen@gmail.com>
2023-09-20 11:49:34 -0600

    gfchbjlmk

---
+++ ConversionSite/scripts/convert_temp.js
@@ -0,0 +1,38 @@
+window.addEventListener("DOMContentLoaded", domLoaded);
+
+// When the DOM has finished loading, add the event listeners.
+function domLoaded() {
+   // TODO: Use addEventListener() to register a click event handler for the convert button.
+   // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#add_a_simple_listener
+
+   // Add event listeners to handle clearing the box that WAS NOT clicked,
+   // e.g., the element C_in listens for 'input', with a callback fn to
+   // execute after that event does happen. 
+   // You don't send arguments to the event handler function.
+   // So, if you want the event handler to call another function that
+   // DOES take arguments, you can send that other function as a callback.
+   // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#event_listener_with_anonymous_function
+   // Here is an example of anonymous event handler fn that calls alert with an argument:
+   // document.getElementById("weatherIcon").addEventListener("click", function() {alert("You clicked the icon.")})
+
+}
+// TODO: (Part of the above is to write the functions to be executed when the event handlers are invoked.)
+
+function convertCtoF(C) {
+   // TODO: Return temp in °F. 
+   // °F = °C * 9/5 + 32
+}
+
+function convertFtoC(F) {
+   // TODO: Return temp in °C. 
+   // °C = (°F - 32) * 5/9
+}
+
+// TODO: write a fn that can be called with every temp conversion
+// to display the correct weather icon.
+// Based on degrees Fahrenheit:
+// 32 or less, but above -200: cold
+// 90 or more, but below 200: hot
+// between hot and cold: cool
+// 200 or more, -200 or less: dead
+// both input fields are blank: C-F
