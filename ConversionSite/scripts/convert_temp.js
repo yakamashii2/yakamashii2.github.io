57869997e27b547172e28976da67a4f207bf98d8
Edward Aldeen <edward.aldeen@gmail.com>
2023-09-21 12:57:07 -0600

    mnbmjhbjhbjhb

--- ConversionSite/scripts/convert_temp.js
+++ ConversionSite/scripts/convert_temp.js
@@ -21,14 +21,37 @@ function domLoaded() {
       document.getElementById("C_in").value = "";
    });
    document.getElementById("convertButton").addEventListener("click", function(){
-      if(boolF == false && boolC == false){
+      if(document.getElementById("F_in").value == "" && document.getElementById("C_in").value == ""){
          document.getElementById("message").innerHTML = "Enter a temperature to convert,";
          //put in a value monkey god damn
          document.getElementById("C_in").value = "";
          document.getElementById("F_in").value = "";
+         boolC = false;
+         boolF = false;
+         document.getElementById("weatherIcon").src = "images/C-F.png"
+      }else if(boolF == false && boolC == false){
+         document.getElementById("message").innerHTML = "Enter a temperature to convert,";
+         //put in a value monkey god damn
+         document.getElementById("C_in").value = "";
+         document.getElementById("F_in").value = "";
+         boolC = false;
+         boolF = false;
+         document.getElementById("weatherIcon").src = "images/C-F.png"
       }else if(boolF == true){
          document.getElementById("message").innerHTML = "";
          //convert F to C
+         //you can have modular clean code, never.
+      if(document.getElementById("F_in").value <= 32 && document.getElementById("F_in").value > 200){
+         document.getElementById("weatherIcon").src = "images/cold.png"
+      }else if(document.getElementById("F_in").value >= 90 && document.getElementById("F_in").value < 200){
+         document.getElementById("weatherIcon").src = "images/hot.png"
+      }else if(document.getElementById("F_in").value > 32 && document.getElementById("F_in").value < 90){
+         document.getElementById("weatherIcon").src = "images/cool.png"
+      }else if(document.getElementById("F_in").value >= 200 || document.getElementById("F_in").value <= 200){
+         document.getElementById("weatherIcon").src = "images/dead.png"
+      }else if(document.getElementById("F_in").value == "" && document.getElementById("C_in").value == ""){
+         document.getElementById("weatherIcon").src = "images/C-F.png"
+      }
          document.getElementById("C_in").value = convertCtoF(document.getElementById("F_in").value);
          document.getElementById("F_in").value = "";
          boolC = true;
@@ -38,12 +61,25 @@ function domLoaded() {
          //convert C to F
          document.getElementById("F_in").value = convertFtoC(document.getElementById("C_in").value);
          document.getElementById("C_in").value = "";
+         //you can have modular clean code, never.
+      if(document.getElementById("F_in").value <= 32 && document.getElementById("F_in").value > 200){
+         document.getElementById("weatherIcon").src = "images/cold.png"
+      }else if(document.getElementById("F_in").value >= 90 && document.getElementById("F_in").value < 200){
+         document.getElementById("weatherIcon").src = "images/hot.png"
+      }else if(document.getElementById("F_in").value > 32 && document.getElementById("F_in").value < 90){
+         document.getElementById("weatherIcon").src = "images/cool.png"
+      }else if(document.getElementById("F_in").value >= 200 || document.getElementById("F_in").value <= 200){
+         document.getElementById("weatherIcon").src = "images/dead.png"
+      }else if(document.getElementById("F_in").value == "" && document.getElementById("C_in").value == ""){
+         document.getElementById("weatherIcon").src = "images/C-F.png"
+      }
          boolF = true;
          boolC = false;
       }else {
          document.getElementById("message").innerHTML = "";
          //brick
       }
+
    } );
    // Add event listeners to handle clearing the box that WAS NOT clicked,
    // e.g., the element C_in listens for 'input', with a callback fn to
