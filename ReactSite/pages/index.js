3ce810cbcd96b939af3be2a707a9b9e512ac5794
Edward Aldeen <84109198+yakamashii2@users.noreply.github.com>
2023-11-06 15:36:43 -0700

    Update index.js

--- ReactSite/pages/index.js
+++ ReactSite/pages/index.js
@@ -4,6 +4,7 @@ function HomePage() {
   return (
     <div>
       <h1>Welcome to My Next.js App</h1>
+	  <a href="https://github.com/yakamashii2/yakamashii2.github.io/tree/main/ReactSite">https://github.com/yakamashii2/yakamashii2.github.io/tree/main/ReactSite</a>
     </div>
   );
 }
